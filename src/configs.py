fe1ee831fda1c2e4534b0764484f787f15c6e82b
n.rurenko <eavtorsky@gmail.com>
2023-01-16 22:13:12 +0300

    fix(./src/): syntax improvements after code review

--- src/configs.py
+++ src/configs.py
@@ -5,28 +5,47 @@ from logging.handlers import RotatingFileHandler
 from constants import BASE_DIR
 
 DT_FORMAT = '%d.%m.%Y %H:%M:%S'
+LOG_DIR = 'logs'
 LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
+PARSER_CACHE_FLAGS = (
+    '-c',
+    '--clear-cache',
+)
+PARSER_CACHE_HELP = 'Очистка кеша'
+PARSER_DESC_HELP = 'Парсер документации Python'
+PARSER_MODE_FLAGS = ('mode',)
+PARSER_MODE_HELP = 'Режимы работы парсера'
+PARSER_OUTPUT_FLAGS = (
+    '-o',
+    '--output',
+)
+PARSER_OUTPUT_HELP = 'Дополнительные способы вывода данных'
 
 
 def configure_argument_parser(available_modes):
-    parser = argparse.ArgumentParser(description='Парсер документации Python')
+    parser = argparse.ArgumentParser(description=PARSER_DESC_HELP)
     parser.add_argument(
-        'mode', choices=available_modes, help='Режимы работы парсера'
+        PARSER_MODE_FLAGS[0],
+        choices=available_modes,
+        help=PARSER_MODE_HELP,
     )
     parser.add_argument(
-        '-c', '--clear-cache', action='store_true', help='Очистка кеша'
+        PARSER_CACHE_FLAGS[0],
+        PARSER_CACHE_FLAGS[1],
+        action='store_true',
+        help=PARSER_CACHE_HELP,
     )
     parser.add_argument(
-        '-o',
-        '--output',
+        PARSER_OUTPUT_FLAGS[0],
+        PARSER_OUTPUT_FLAGS[1],
         choices=('pretty', 'file'),
-        help='Дополнительные способы вывода данных',
+        help=PARSER_OUTPUT_HELP,
     )
     return parser
 
 
 def configure_logging():
-    log_dir = BASE_DIR / 'logs'
+    log_dir = BASE_DIR / LOG_DIR
     log_dir.mkdir(exist_ok=True)
     log_file = log_dir / 'parser.log'
 
